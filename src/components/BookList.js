c40c505a802a9cb185f1dd5a441a1f4b0df8f0ac
kimseungwoo449 <ehdanf449@naver.com>
2024-05-14 11:46:36 +0900

    Add Library : react-icons

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,4 +1,12 @@
+import { Box, Icon, Heading, TableContainer, Table, Tbody, Thead, Tfoot, Tr, Th, Td, Button, ButtonGroup, 
+    Stack, HStack, VStack,IconButton, 
+    useColorMode,
+    useColorModeValue} from '@chakra-ui/react';
 import React, { useState, useEffect, useRef } from 'react';
+import { Input } from '@chakra-ui/react'
+import { FaYoutube } from "react-icons/fa6";
+import { FaMoon } from "react-icons/fa";
+import { FaSun } from "react-icons/fa";
 
 const BookList = () => {
     // const [count,setCount] = useState(1);
@@ -9,13 +17,17 @@ const BookList = () => {
     const [search, setSearch] = useState('강아지똥');
     // useRef 는 화면 랜더링 반영되지 않는 참조값
     const pageCount = useRef(1);
-    
+
+    // Chakra UI 에서 제공하는 훅
+    const {colorMode,toggleColorMode} = useColorMode();
+    const color = useColorModeValue('red.500', 'red.200');
+    const buttonScheme = useColorModeValue("blackAlpha","whiteAlpha");
 
     const fetchBooks = async () => {
         const response = await fetch(
             `https://dapi.kakao.com/v2/search/vclip?query=${search}&page=${page}`,
-            {   
-                method : "GET",
+            {
+                method: "GET",
                 headers: {
                     Authorization: `KakaoAK ${process.env.REACT_APP_API_KEY}`,
                 }
@@ -24,18 +36,18 @@ const BookList = () => {
         const data = await response.json();
         console.log(data);
 
-        pageCount.current = data.meta.pageable_count % 10 > 0 
-        ? data.meta.pageable_count / 10 + 1 
-        : data.meta.pageable_count / 10;
+        pageCount.current = data.meta.pageable_count % 10 > 0
+            ? data.meta.pageable_count / 10 + 1
+            : data.meta.pageable_count / 10;
         pageCount.current = Math.floor(pageCount.current);
-        pageCount.current =  pageCount.current > 15 ? 15 : pageCount.current;
+        pageCount.current = pageCount.current > 15 ? 15 : pageCount.current;
         console.log(pageCount);
 
 
         setBookList(data.documents);
     };
-    
-    const changeSearch = e=>{
+
+    const changeSearch = e => {
         // if(e.keyCode==13){
         //     // let searchValue = document.getElementById('search-value').value;
         //     // document.getElementById('search-value').value = '';
@@ -43,7 +55,7 @@ const BookList = () => {
         //     e.taget.value = '';
         // }
 
-        if(e.target.value.length>1){
+        if (e.target.value.length > 1) {
             setSearch(e.target.value);
         }
     };
@@ -54,22 +66,57 @@ const BookList = () => {
 
     return (
         <>
-            <h1>동영상 검색 목록</h1>
-            <div>
-                <input type='text' placeholder='검색할 내용' id='search-value' onChange={changeSearch}></input>
-                {bookList.map(book => (
-                    <>
-                        <p>{book.title}</p>
-                    </>
-                ))}
-            </div>
-            <ul>
+            <Heading color={color}>
+                <Icon as={FaYoutube} boxSize={"1.5em"} />
+                동영상 검색 목록
+            </Heading>
+            {
+                colorMode === "light"?
+                <IconButton icon={<FaMoon/>} onClick={toggleColorMode} size={"lg"} /> :
+                <IconButton icon={<FaSun/>} onClick={toggleColorMode} size={"lg"} />
+            }
+            <Box>
+                <Input type='text' placeholder='검색할 내용' id='search-value' onChange={changeSearch} size='lg' variant='filled'>
+                </Input>
+                <TableContainer>
+                    <Table size='sm' colorScheme="teal" variant='striped'>
+                        <Thead>
+                            <Tr>
+                                <Th>No</Th>
+                                <Th>Title</Th>
+                                <Th>Author</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            {bookList.map((book, index) => (
+                                <>
+                                    <Tr>
+                                        <Td>{(page - 1) * 10 + index + 1}</Td>
+                                        <Td><a href={book.url}>{book.title}</a></Td>
+                                        <Td>{book.author}</Td>
+                                    </Tr>
+                                </>
+                            ))}
+                        </Tbody>
+                    </Table>
+                </TableContainer>
+            </Box>
+
+
+            <HStack>
                 {Array.from({ length: pageCount.current }, (_, index) => (
                     <>
-                        <li onClick={e => {setPage(index + 1)}}>{index + 1}</li >
+                        <Button colorScheme={
+                            page === index+1?
+                            "red":buttonScheme
+                        } onClick={e => { setPage(index + 1) }}>
+                            {index + 1}
+                        </Button>
+
                     </>
                 ))}
-        </ul >
+            </HStack>
+
         </>
     );
 };
